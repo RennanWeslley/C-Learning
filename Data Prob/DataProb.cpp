77d5ba9052a0fa7b7d81f0d18612a3a1c4ac9bda
Rennan Weslley <rennanweslley18@gmail.com>
2017-11-10 04:44:09 -0200

    Fixing for virgins can compile it

--- Data Prob/DataProb.cpp
+++ Data Prob/DataProb.cpp
@@ -14,37 +14,43 @@ void drawTable(vector<Data>, int, int, int, int);
 
 int main() {
     int L = 0;
-    
+
     float sumFreqRel,
           sumFreqRelPer;
-        
+
     sumFreqRel = sumFreqRelPer = 0;
-        
+
     vector<Data> data;
     vector<int> dataN;
     vector<int> dataName;
     Data *dataT;
-    
+
     /* Recieving user data files */
     for(string s; 1; L++) {
-        cout << "Digite o " << (L+1) << "º dado ou \"exit\" para sair: ";
+        cout << "Digite o " << (L+1) << "\xa7" << " dado ou \"exit\" para sair: ";
         cin >> s;
         cin.ignore();
-        
+
         if(!s.compare("exit"))
             break;
-        
-        dataN.push_back(stoi(s));         // stoi() func. needs a C11 compiler
+
+        /* Conv. string to int */
+        stringstream intStr(s);
+        int auxIntStr;
+        intStr >> auxIntStr;
+        /* End Of Conversion */
+
+        dataN.push_back(auxIntStr);
         sort(dataN.begin(), dataN.end()); // Sorting the vector dataN
     }
     /* End Of Recieving Data */
-    
+
     /* Setting Name */
     for(int i = 0; i < dataN.size(); i++)
         if(!search(dataName, dataN[i]))
             dataName.push_back(dataN[i]);
     /* End Of Set Name */
-    
+
     /* Setting Atributes */
     for(int i = 0; i < dataName.size(); i++) {
         dataT = new Data();
@@ -54,7 +60,7 @@ int main() {
         dataT->calcFreqRelPer(L);
         data.push_back(*dataT);
         data[i].calcFreqRelAc(data, i);
-        
+
         /* Sum */
         sumFreqRel    += dataT->getFreqRel();
         sumFreqRelPer += dataT->getFreqRelPer();
@@ -64,6 +70,8 @@ int main() {
 
     /* Drawing Table */
     drawTable(data, dataName.size(), dataN.size(), sumFreqRel, sumFreqRelPer);
+
+    return 0;
 }
 
 /* Returns true if the num was on the vector data otherwise returns false */
@@ -72,18 +80,18 @@ bool search(vector<int> data, int num) {
         if(num == data[i])
             return true;
     }
-    
+
     return false;
 }
 
 /* Returns the index of the num in the vector data */
 int numOf(vector<int> data, int num) {
     int j = 0;
-    
+
     for(int i = 0; i < data.size(); i++)
         if(num == data[i])
             j++;
-        
+
     return j;
 }
 
@@ -93,23 +101,23 @@ void drawTable(vector<Data> data, int L, int sumFreqAbs, int sumFreqRel, int sum
                  sumRel,
                  sumPer,
                  sumAc;
-                 
+
     sumAbs << setw(3) << sumFreqAbs;
     sumRel << setw(3) << sumFreqRel;
     sumPer << setw(3) << setfill('0') << sumFreqRelPer;
     sumAc  << setw(3) << data[data.size() - 1].getFreqRelAc();
-    
+
     cout << endl;
     for(int i = 0; i < COL; i++)
         cout << lineData[i];
 
     cout << endl;
-    for(int i = 0; i < L; i++) 
+    for(int i = 0; i < L; i++)
         cout << data[i].stringTable() << endl;
-    
+
     cout << "      Total          " << sumAbs.str()
          << "           "           << sumRel.str()
          << "           "           << sumPer.str()
-         << "              "        << sumAc.str() 
+         << "              "        << sumAc.str()
          << endl << endl;
 }
