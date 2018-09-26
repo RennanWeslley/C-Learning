7f50d0387eafebfbcb93ce60b19021bf854039d3
Rennan Weslley <rennanweslley18@gmail.com>
2018-09-26 12:58:35 -0300

    Leitura de arquivos

---
+++ arquivo.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <sstream>
+
+int main() {
+    FILE *f = fopen("dij20.txt", "r");
+
+    if(!f) {
+        printf("Arquivo nao pode ser aberto.\n");
+        return -1;
+    }
+
+    std::string s;
+    char c;
+    int sz;
+
+    while(1) {
+        c = getc(f);
+
+        if(c == '\n')
+            break;
+
+        s += c;
+    }
+
+    std::istringstream num(s);
+
+    num >> sz;
+    sz--;
+
+    int arr[sz][sz];
+
+    for(int i = 0; i < sz; i++)
+        for(int j = 0; j < sz; j++)
+            arr[i][j] = 0;
+
+    s = "";
+
+    for(int i = 0, j = 0, n; i < sz; j = 0, n = 0, s = "", i++) {
+        while(1) {
+            c = getc(f);
+
+            if(c == '\n') {
+                std::istringstream line(s);
+                line >> n;
+
+                arr[i][j] = n;
+
+                break;
+            }
+
+            if(c == ' '){
+                std::istringstream line(s);
+                line >> n;
+
+                arr[i][j] = n;
+
+                n = 0;
+                s = "";
+                j++;
+
+                continue;
+            }
+
+            s += c;
+        }
+    }
+
+    fclose(f);
+
+    for(int i = 0; i < sz; printf("\n"), i++)
+        for(int j = 0; j < sz; j++)
+            if(arr[i][j])
+                printf("%5d ", arr[i][j]);
+
+    printf("\n\n");
+
+    return 0;
+}
